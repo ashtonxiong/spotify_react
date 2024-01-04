b458f6aec2e6309829281c6bc8b37292a4fe42dc
Ashton Xiong <ashtonxiong12@gmail.com>
2024-01-03 22:03:19 -0500

    made homepage.js

--- server/server.js
+++ server/server.js
@@ -1,25 +1,25 @@
-// server.js (Express)
-const express = require("express");
-const querystring = require("querystring");
-const { generateRandomString } = require("./your-utils-file");
+// // server.js (Express)
+// const express = require("express");
+// const querystring = require("querystring");
+// const { generateRandomString } = require("./your-utils-file");
 
-const app = express();
+// const app = express();
 
-const CLIENT_ID = "e6b84f273b7b44e082fb7fd319d4a217";
-const REDIRECT_URI = "http://localhost:3000";
-const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
-const RESPONSE_TYPE = "token";
-const redirectUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
+// const CLIENT_ID = "e6b84f273b7b44e082fb7fd319d4a217";
+// const REDIRECT_URI = "http://localhost:3000";
+// const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize";
+// const RESPONSE_TYPE = "token";
+// const redirectUrl = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
 
-app.get(redirectUrl, (req, res) => {
-  const state = generateRandomString(16);
-  const scope = "user-read-private user-read-email";
+// app.get(redirectUrl, (req, res) => {
+//   const state = generateRandomString(16);
+//   const scope = "user-read-private user-read-email";
 
-  let data = res.redirect(redirectUrl);
-  console.log(data);
-});
+//   let data = res.redirect(redirectUrl);
+//   console.log(data);
+// });
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,1 +1,25 @@
-import { useEffect, useState } from "react";
+import { React, useEffect, useState } from "react";
+import axios from "axios";
+
+const HomePage = () => {
+  const getProfile = async () => {
+    let token = localStorage.getItem("accessToken");
+    console.log(token);
+
+    const response = await axios.get("https://api.spotify.com/v1/me", {
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    });
+    console.log("TEST PROFILE", response);
+  };
+
+  return (
+    <div>
+      TEST
+      <button onClick={getProfile}></button>
+    </div>
+  );
+};
+
+export default HomePage;
